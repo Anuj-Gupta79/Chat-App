efcba46d7bee04272b3ae7900d4ee9b50a176395
anuj <anujgupta7906@gmail.com>
2022-11-05 14:01:51 +0530

    registration page

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Register } from "./Pages/Register";
 import "./style.scss"
 function App() {
--- chat/src/Pages/Register.jsx
+++ chat/src/Pages/Register.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from "react";
+import profile from "../Image/Image_upload.png";
 
 export const Register = () => {
   return (
-    <div className='formContainer'>
-        <div className='formWrapper'>
-            <span className="Logo">Apne Log</span>
-            <span className="title">Register</span>
-            <form>
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input type="file" />
-                <button>Sign up</button>
-            </form>
-            <p>Already have an account? Login</p>
-        </div>
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="Logo">Apne Log</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={profile}></img>
+            <span>Add Profile Picture</span>
+          </label>
+          <button>Sign up</button>
+        </form>
+        <p>Already have an account? Login</p>
+      </div>
     </div>
-  )
-}
+  );
+};
