bf1cc899dda8495b6ad99784e29a131a0d9ec8bb
anuj <anujgupta7906@gmail.com>
2022-11-08 01:26:51 +0530

    commit

--- chat/src/Components/Chat.jsx
+++ chat/src/Components/Chat.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import add from "../Image/add.png";
+import cam from "../Image/cam.png";
+import more from "../Image/more.png";
+import Messages from './Messages';
+import Input from "./input";
+
 
 const Chat = () => {
   return (
-    <div className="chat">Chat</div>
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Admin</span>
+        <div className="icon">
+          <img src={cam}></img>
+          <img src={add}></img>
+          <img src={more}></img>
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
   )
 }
 
--- chat/src/Components/input.jsx
+++ chat/src/Components/input.jsx
@@ -1,9 +1,25 @@
-  import React from 'react'
+import React from 'react';
+import attachment from "../Image/attach.png";
+import img from "../Image/img.png";
+import images from "../Image/images.png";
   
-  const input = () => {
-    return (
-      <div>input</div>
-    )
-  }
+const input = () => {
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type Something...."/>
+      <div className="inputIcons">
+        <img src={attachment}/>
+        <input type="file" style={{display:"none"}} id="file"/>
+        <label htmlFor="file">
+           <img src={img}/>
+        </label>
+        <button>
+          Send
+          {/* <img src={images}/> */}
+        </button>
+      </div>
+    </div>
+  )
+}
   
-  export default input
+export default input
