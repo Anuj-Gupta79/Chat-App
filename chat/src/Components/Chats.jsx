c1042199d4be55c04938b465ffeab4a92a9bbd62
anuj <anujgupta7906@gmail.com>
2022-11-08 14:39:12 +0530

    commit

--- chat/src/Components/Chats.jsx
+++ chat/src/Components/Chats.jsx
@@ -58,6 +58,8 @@ const Chats = () => {
           <p>Hello! How are you?</p>
         </div>
       </div>
+
+      
       
     </div>
   );
--- chat/src/Components/Message.jsx
+++ chat/src/Components/Message.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message owner">
+      <div className="userInfo">
+        <img src="https://www.pngmart.com/files/21/Admin-Profile-Vector-PNG-Image.png" />
+        <span>just now</span>
+      </div>
+      <div className="userContent">
+        <p>Hello</p>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png" /> 
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
